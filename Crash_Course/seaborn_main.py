e8dbbdb8a6ad61a353c53dcd9e6a72001f7d0540
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-07-02 03:14:03 +0530

    [Shubham][2-July] Python Crash Course - Seaborn

---
+++ Crash_Course/seaborn_main.py
@@ -0,0 +1,65 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy
+
+tips = sns.load_dataset('tips')
+flights = sns.load_dataset('flights')
+print(tips.head())
+
+#Histogram
+#sns.displot(tips['total_bill'])
+
+#Scatter Plot
+#sns.jointplot(x='total_bill',y='tip',data=tips, kind='reg')
+#plt.show()
+
+#Pairplot for all numerical column values
+#sns.pairplot(tips,hue='sex') #hue is for categorical column
+#plt.show()
+
+#rugplot marka dash plot throughout the single line
+#sns.rugplot(tips['total_bill'])
+#plt.show()
+
+#barplot
+#sns.barplot(x='sex',y='total_bill',data=tips, estimator=np.std)
+
+#count_plot
+#sns.countplot(x='sex',data=tips)
+
+#candlecharts
+#sns.boxplot(x='day',y='total_bill',data=tips)
+
+#violinplot
+#sns.violinplot(x='day',y='total_bill', data=tips, hue='sex')
+
+#stripplot
+#sns.stripplot(x='day',y='total_bill',data=tips, jitter=True)
+
+
+#stripplot + violin
+#sns.swarmplot(x='day',y='total_bill',data=tips)
+
+#df=pd.DataFrame(tips)
+
+#pivot_table = df.pivot_table(index='sex', columns='day', values='total_bill', aggfunc='sum')
+
+# Create heatmap
+#plt.figure(figsize=(10, 6))
+#sns.heatmap(pivot_table, annot=True, cmap='YlGnBu', fmt='.2f')
+
+# Set titles and labels
+#plt.title('Total Bill Heatmap')
+#plt.xlabel('Day')
+#plt.ylabel('Sex')
+
+df=pd.DataFrame(flights)
+
+pivot_table = df.pivot_table(index='month', columns='year', values='passengers')
+#sns.heatmap(pivot_table)
+
+sns.clustermap(pivot_table)
+plt.show()
+
