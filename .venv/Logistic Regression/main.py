4c6ee51bb6eeaf19ca37dd21f264898be59cde27
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-07-17 23:38:23 +0530

    [Shubham][17-July] Logistic Expression

--- .venv/Logistic Regression/main.py
+++ .venv/Logistic Regression/main.py
@@ -16,8 +16,41 @@ sns.set_style('whitegrid')
 
 #sns.countplot(x='SibSp',data=train)
 
-train['Fare'].hist(bins=40,figsize=(10,4))
-plt.show()
+#train['Fare'].hist(bins=40,figsize=(10,4))
+#sns.boxplot(x='Pclass',y='Age',data=train)
+
+def impute_age(cols):
+    a=cols.iloc[0]
+    p=cols.iloc[1]
+
+    if pd.isnull(a):
+        if p==1:
+            return 37
+        elif p==2:
+            return 29
+        else:
+            return 24
+    else:
+        return a
+
+train['Age']=train[['Age','Pclass']].apply(impute_age,axis=1)
+#sns.heatmap(data=train.isnull(),yticklabels=False,cmap='viridis',cbar=False)
+
+train.drop('Cabin',axis=1,inplace=True)
+train['Age']=train[['Age','Pclass']].apply(impute_age,axis=1)
+sns.heatmap(data=train.isnull(),yticklabels=False,cmap='viridis',cbar=False)
+
+
+sex=pd.get_dummies(train['Sex'],drop_first=True)
+emb=pd.get_dummies(train['Embarked'],drop_first=True)
+train=pd.concat([train,sex,emb],axis=1)
+
+train.drop(['Sex','Embarked','Name','Ticket','PassengerId'], axis=1,inplace=True)
+
+print(train.head())
+
+
+
 
 
 
