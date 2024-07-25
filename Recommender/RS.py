91e9d4f2c2a74272e5062f537264e6a4400c59d4
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-07-26 02:34:50 +0530

    [Shubham][26-July] Recommender System

--- Recommender/RS.py
+++ Recommender/RS.py
@@ -20,6 +20,25 @@ ratings['num of ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
 
 #ratings['rating'].hist(bins=70)
 
-sns.jointplot(x='rating',y='num of ratings',data=ratings)
-plt.show()
+#sns.jointplot(x='rating',y='num of ratings',data=ratings)
+
+moviemat=df.pivot_table(index='user_id',columns='title',values='rating')
+#print(ratings.sort_values('num of ratings',ascending=False))
+
+sw_ratings = moviemat['Star Wars (1977)']
+ll_ratings = moviemat['Liar Liar (1997)']
+
+sw_similar=moviemat.corrwith(sw_ratings)
+ll_similar=moviemat.corrwith(ll_ratings)
+
+corr_sw=pd.DataFrame(sw_similar,columns=['Correlation'])
+corr_ll=pd.DataFrame(ll_similar,columns=['Correlation'])
+
+corr_sw=corr_sw.join(ratings['num of ratings'])
+corr_sw[corr_sw['num of ratings']>100].sort_values('Correlation',ascending=False).head()
+
+print(corr_sw[corr_sw['num of ratings']>100].sort_values('Correlation',ascending=False).head())
+
+corr_ll=corr_ll.join(ratings['num of ratings'])
+print(corr_ll[corr_ll['num of ratings']>200].sort_values('Correlation',ascending=False).head())
 
